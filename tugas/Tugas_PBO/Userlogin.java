4c80d491eb1da37c97e7a686a2360212b22a1b52
RezhaRivai <rezharivai06@gmail.com>
2025-03-17 23:04:16 +0700

    Update

--- tugas/Tugas_PBO/Userlogin.java
+++ tugas/Tugas_PBO/Userlogin.java
@@ -1,6 +1,38 @@
 import java.util.Scanner;
 
-public class Userlogin {
+class Userlogin {
+    public class admin{
+         String Username;
+         int Password;
+    }
+    public void login(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nFormat\nAdminxxx\nPasswordxxx");
+        System.out.print("Masukkan username: ");
+        String username = input.nextLine();
+        System.out.print("\nMasukkan password: ");
+        String password = input.nextLine();
+
+        String nimAkhir = "064";
+        String validUsername = "Admin" + nimAkhir;
+        String validPassword = "Password" + nimAkhir;
+
+        if (username.equals(validUsername) && password.equals(validPassword)) {
+            System.out.println("Login Admin berhasil!");
+        } else {
+            System.out.println("Login anda gagal! Username atau pasaword salah");
+        }
+
+    }
+    public class mahasiswa{
+        String Username;
+        int Password;
+    }
+
+    public void login(){
+
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Pilih Jenis Login:");
@@ -12,21 +44,7 @@ public class Userlogin {
         input.nextLine();
 
         if (pilihan == 1) {
-            System.out.println("\nFormat\nAdminxxx\nPasswordxxx");
-            System.out.print("Masukkan username: ");
-            String username = input.nextLine();
-            System.out.print("\nMasukkan password: ");
-            String password = input.nextLine();
-
-            String nimAkhir = "064";
-            String validUsername = "Admin" + nimAkhir;
-            String validPassword = "Password" + nimAkhir;
-
-            if (username.equals(validUsername) && password.equals(validPassword)) {
-                System.out.println("Login Admin berhasil!");
-            } else {
-                System.out.println("Login anda gagal! Username atau pasaword salah");
-            }
+            login();
         } else if (pilihan == 2) {
             System.out.print("Masukkan Nama: ");
             String nama = input.nextLine();
@@ -37,7 +55,8 @@ public class Userlogin {
             String validNim = "202410370110064";
 
             if (nama.equals(validNama) && nim.equals(validNim)) {
-                System.out.println("\nLogin Mahasiswa berhasil!");
+                System.out.println();
+                System.out.println("Login Mahasiswa berhasil!");
                 System.out.println("Nama: " + nama);
                 System.out.println("NIM: " + nim);
             } else {
